2502f9434b738e64e392d280b78d2c16675997a0
buoyantichub <nsikandonatusekpo@gmail.com>
2023-08-23 04:10:02 -0700

    Updated

--- functions1.c
+++ functions1.c
@@ -95,7 +95,7 @@ int print_hexadecimal(va_list types, char buffer[],
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
  * @types: List of arguments
  * buffer: Buffer array to handle print
- * @flags: Calculates active flags
+ * @flags: Calculate active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
